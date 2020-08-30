83796897816cddd2d746f9f3ae5a48574d4b16f2
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-08-30 17:32:14 +0500

    Fix package json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const server = express()
 
 server.get('/', (req, res) => {
-	res.send('Salom bu index')
+	res.send('Salom bu index').end()
 })
 
-server.listen(process.env.port, () => console.log('Ready at 4000'))
+server.listen(process.env.port || 4000, () => console.log('Ready at 4000'))
