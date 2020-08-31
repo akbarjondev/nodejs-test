759dc20192d94c58428912a913b5d5dff8e31b71
AkbarjonTojiyev <tojiyev.akbarjon@gmail.com>
2020-08-31 08:31:04 +0500

    Refactor code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const server = express()
 
 server.get('/', (req, res) => {
-	res.send('Salom bu index').end()
+	res.send('Salom bu index emas').end()
 })
 
 server.listen(process.env.PORT || 8080, () => console.log('Ready at 4000'))
